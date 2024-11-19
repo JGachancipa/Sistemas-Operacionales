bb97eff3fb18c236d43c00fab3f5aceb4213b21b
JGachancipa <150005302+JGachancipa@users.noreply.github.com>
2024-11-18 19:28:47 -0500

    Update WebConfig.java

--- backend-operacionales/api/src/main/java/com/politecnico/operacionales/api/config/WebConfig.java
+++ backend-operacionales/api/src/main/java/com/politecnico/operacionales/api/config/WebConfig.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private static final String ALLOWED_ORIGIN = "http://operacionales-front-3e6297ebbc2e.herokuapp.com";
+    private static final String ALLOWED_ORIGIN = "https://operacionales-front-3e6297ebbc2e.herokuapp.com";
     private static final String[] ALLOWED_METHODS = {"GET", "POST", "OPTIONS"};
     private static final String[] ALLOWED_HEADERS = {"Authorization", "Content-Type", "Accept"};
 
     @Override
-    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins(ALLOWED_ORIGIN)
                 .allowedMethods(ALLOWED_METHODS)
